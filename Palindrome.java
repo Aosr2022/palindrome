851b3ac8ddd267c62541c45aa5f52f25d45416d2
lrahmaosr <90198302+lrahmaosr@users.noreply.github.com>
2021-10-27 11:22:33 +0200

    Initial commit

---
+++ Palindrome.java
@@ -0,0 +1,36 @@
+ import java.util.*; 
+
+public class Palindrome
+
+{ 
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Enter the text: ");
+		String text = input.nextLine(); 
+		System.out.println(isPalindrome(text));
+
+		// Complete and use the methods below to 
+		// determine if "text" is a palindrome.
+
+	}
+
+	// Check if a string is a palindrome
+	public static boolean isPalindrome(String str)
+	{  
+		return true;
+	}
+
+	// Return the string with the spaces removed
+	public static String removeSpaces(String str)
+	{  
+		return "hello";
+	}
+
+	// Return the string reversed.
+	public static String reverseString(String str)
+	{  
+		return "hello";
+	}
+
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# Palindrome Assignment
+## Work with a partner
+
+Complete the methods to determine if given text is a palindrome.
